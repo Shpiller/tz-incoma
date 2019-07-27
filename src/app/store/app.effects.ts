e58a51042fd77f91f2f455b8d5cbc429647f32b9
r.alexey <r.alexey.mail@gmail.com>
2019-07-27 22:51:04 +0400

    Rewrite switchMap for cancel request when query empty

--- src/app/store/app.effects.ts
+++ src/app/store/app.effects.ts
@@ -1,8 +1,8 @@
 import {Injectable} from '@angular/core';
 import {Actions, createEffect, ofType} from '@ngrx/effects';
 import {AppActions, AppActionsTypes} from './app.actions';
-import {catchError, filter, map, switchMap} from 'rxjs/operators';
-import {EMPTY} from 'rxjs';
+import {catchError, first, map, switchMap} from 'rxjs/operators';
+import {EMPTY, of} from 'rxjs';
 import {BooksInterfaces} from '../modules/books/interfaces/books.interfaces';
 import {BooksService} from '../services/books/books.service';
 
@@ -16,28 +16,19 @@ export class AppEffects {
         this.actions$.pipe(
             ofType(AppActionsTypes.GET_BOOKS),
             map((action: AppActions.GetBooks) => action.payload),
-            filter(query => !!query),
             switchMap(query => {
 
-                return this.booksService.getBooks(query)
-                    .pipe(
-                        map(books => {
-                            return new AppActions.GetBooksSuccess(books as BooksInterfaces.IListResponse);
-                        }),
-                        catchError(() => EMPTY),
-                    );
-            })
-        )
-    );
-
-    clearBooks$ = createEffect(() =>
-        this.actions$.pipe(
-            ofType(AppActionsTypes.GET_BOOKS),
-            map((action: AppActions.GetBooks) => action.payload),
-            filter(query => !query),
-            map(query => {
-
-                return new AppActions.GetBooksSuccess(null);
+                if (!query) {
+                    return of(new AppActions.GetBooksSuccess(null)).pipe(first());
+                } else {
+                    return this.booksService.getBooks(query)
+                        .pipe(
+                            map(books => {
+                                return new AppActions.GetBooksSuccess(books as BooksInterfaces.IListResponse);
+                            }),
+                            catchError(() => EMPTY),
+                        );
+                }
             })
         )
     );
