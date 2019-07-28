2b508354664a2acb55ce5e20c3733570d355e3d6
r.alexey <r.alexey.mail@gmail.com>
2019-07-28 10:51:36 +0400

    Show progress status for search

--- src/app/modules/books/books.component.ts
+++ src/app/modules/books/books.component.ts
@@ -5,6 +5,7 @@ import {AppStore} from '../../store/app.store';
 import {Subscription} from 'rxjs';
 import {map} from 'rxjs/operators';
 import {AppActions} from '../../store/app.actions';
+import {CommonInterfaces} from '../../interfaces/common.interfaces';
 
 @Component({
     selector: 'app-books',
@@ -16,11 +17,16 @@ export class BooksComponent implements OnInit, OnChanges, OnDestroy {
 
     query: string;
     booksResponse: BooksInterfaces.IListResponse;
+    serviceLoading: CommonInterfaces.IMapOfBoolean;
 
     private subscriptions: Subscription[] = [];
 
     set subs(sub) {
         this.subscriptions.push(sub);
+    }
+
+    get SERVICE_LOADING() {
+        return AppStore.SERVICE_LOADING;
     }
 
     constructor(private store$: Store<AppStore.IState>,
@@ -53,6 +59,14 @@ export class BooksComponent implements OnInit, OnChanges, OnDestroy {
             select(AppStore.Selects.app.booksResponse),
             map(books => {
                 this.booksResponse = books;
+                this.cdr.detectChanges();
+            }),
+        ).subscribe();
+
+        this.subs = this.store$.pipe(
+            select(AppStore.Selects.app.serviceLoading),
+            map(serviceLoading => {
+                this.serviceLoading = serviceLoading;
                 this.cdr.detectChanges();
             }),
         ).subscribe();
