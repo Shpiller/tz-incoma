2da396067894fc348a07b3147e3b15ec471ecff9
r.alexey <r.alexey.mail@gmail.com>
2019-07-28 20:58:56 +0400

    Show count of favourites

--- src/app/modules/navigation/navigation.component.ts
+++ src/app/modules/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {FavouritesService} from '../../services/favourites/favourites.service';
 
 @Component({
     selector: 'app-navigation',
@@ -7,7 +8,11 @@ import {Component, OnInit} from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-    constructor() {
+    get countFavourites() {
+        return this.favouritesService.favourites.length;
+    }
+
+    constructor(private favouritesService: FavouritesService) {
     }
 
     ngOnInit() {
